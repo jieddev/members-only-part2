c0e7457a92072df2e238d952519be7e3953d89bf
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-02 16:14:06 +0800

    Refactor posts controller to handle user_id by assigning current_user to @post.user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_02_070117) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_02_080826) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_02_070117) do
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
