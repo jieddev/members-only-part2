8111b44ac2e4f6c67a7d8c7e49d80c7a8e29860b
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-02 15:43:22 +0800

    Refactor index.html.erb and new.html.erb to access valid params and display the title and content of existing posts in the index.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,25 @@ class PostsController < ApplicationController
   end
 
   def create 
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
   end
 
-  def new 
+  def new
+    @post = Post.new 
+
   end
+
+
+  private 
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 
 end
